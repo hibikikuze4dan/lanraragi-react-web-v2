fa8a279e7ce2e9c99b661493464accb1bf009ea2
Kibiki Kuze <desu2dan@gmail.com>
2025-05-12 07:16:02 -0500

    chore: code clean transition buttons component logic

--- src/components/end-of-archive-action-buttons/transition-buttons.jsx
+++ src/components/end-of-archive-action-buttons/transition-buttons.jsx
@@ -1,79 +1,17 @@
 import NavigateNext from "@mui/icons-material/NavigateNext";
 import NavigateBefore from "@mui/icons-material/NavigateBefore";
 import { Button, Grid2 } from "@mui/material";
-import useAppPages from "../../hooks/useAppPages";
-import { useRandomArchives } from "../../hooks/useRandomArchives";
-import { useSearchResults } from "../../hooks/useSearchResults";
-import {
-  HISTORY,
-  RANDOM,
-  SCROLL_IMAGES_START_ID,
-  SEARCH,
-} from "../../constants";
-import useCurrentArchive from "../../hooks/useCurrentArchive";
-import { useArchiveHistory } from "../../hooks/useArchiveHistory";
-import {
-  updateCurrentArchiveId,
-  updateImagesScrollTarget,
-} from "../../redux/slices/appSlice";
-import { useDispatch } from "react-redux";
-import { useArchivePages } from "../../hooks/useArchivePages";
-import { putUpdateReadingProgression } from "../../requests/putUpdateReadingProgression";
+import useTransitionButtonsLogic from "./transition-buttons/useTransitionButtonsLogic";
 
 export const TransitionButtons = ({
   gridSize = 12,
   setCurrentPageIndex = () => null,
 }) => {
-  const dispatch = useDispatch();
-  const { archiveOpenedFrom, appPage } = useAppPages();
-  const { getNewArchivePages } = useArchivePages();
-  const { archive } = useCurrentArchive();
-  const { randomArchives } = useRandomArchives();
-  const { addArchiveToHistory } = useArchiveHistory();
-  const { archives: searchArchives } = useSearchResults();
+  const { onReadButtonClick, shouldNotRender } = useTransitionButtonsLogic({
+    setCurrentPageIndex,
+  });
 
-  const isHistoryPage = appPage === HISTORY;
-
-  const pathArchives = {
-    [SEARCH]: searchArchives,
-    [RANDOM]: randomArchives,
-  };
-
-  const archivesToBeSearched = pathArchives?.[archiveOpenedFrom] ?? [];
-
-  const currentArchiveIndex = archivesToBeSearched?.findIndex(
-    (arc) => arc?.arcid === archive?.arcid
-  );
-
-  const dontRender =
-    archiveOpenedFrom === HISTORY || currentArchiveIndex === -1;
-
-  const nextArchive = archivesToBeSearched?.[currentArchiveIndex + 1];
-  const previousArchive = archivesToBeSearched?.[currentArchiveIndex - 1];
-
-  const onReadButtonClick =
-    (isNextArchiveButton = false) =>
-    async () => {
-      if (!isHistoryPage) {
-        addArchiveToHistory(archive);
-      }
-      const archiveToUse = isNextArchiveButton ? nextArchive : previousArchive;
-
-      if (!archiveToUse) {
-        return;
-      }
-
-      const archiveId = archiveToUse?.arcid;
-
-      document.getElementById(SCROLL_IMAGES_START_ID)?.scrollIntoView?.();
-      dispatch(updateCurrentArchiveId(archiveId));
-      setCurrentPageIndex(0);
-      dispatch(updateImagesScrollTarget(""));
-      await getNewArchivePages(archiveId);
-      putUpdateReadingProgression({ archiveId });
-    };
-
-  return dontRender ? null : (
+  return shouldNotRender ? null : (
     <>
       <Grid2 size={gridSize}>
         <Button
---
+++ src/components/end-of-archive-action-buttons/transition-buttons/useTransitionButtonsLogic.js
@@ -0,0 +1,78 @@
+import { useDispatch } from "react-redux";
+import useAppPages from "../../../hooks/useAppPages";
+import { useArchivePages } from "../../../hooks/useArchivePages";
+import useCurrentArchive from "../../../hooks/useCurrentArchive";
+import { useRandomArchives } from "../../../hooks/useRandomArchives";
+import { useArchiveHistory } from "../../../hooks/useArchiveHistory";
+import { useSearchResults } from "../../../hooks/useSearchResults";
+import {
+  HISTORY,
+  RANDOM,
+  SCROLL_IMAGES_START_ID,
+  SEARCH,
+} from "../../../constants";
+import {
+  updateCurrentArchiveId,
+  updateImagesScrollTarget,
+} from "../../../redux/slices/appSlice";
+import { putUpdateReadingProgression } from "../../../requests/putUpdateReadingProgression";
+
+export const useTransitionButtonsLogic = ({
+  setCurrentPageIndex = () => null,
+}) => {
+  const dispatch = useDispatch();
+  const { archiveOpenedFrom, appPage } = useAppPages();
+  const { getNewArchivePages } = useArchivePages();
+  const { archive } = useCurrentArchive();
+  const { randomArchives } = useRandomArchives();
+  const { addArchiveToHistory } = useArchiveHistory();
+  const { archives: searchArchives } = useSearchResults();
+
+  const isHistoryPage = appPage === HISTORY;
+
+  const pathArchives = {
+    [SEARCH]: searchArchives,
+    [RANDOM]: randomArchives,
+  };
+
+  const archivesToBeSearched = pathArchives?.[archiveOpenedFrom] ?? [];
+
+  const currentArchiveIndex = archivesToBeSearched?.findIndex(
+    (arc) => arc?.arcid === archive?.arcid
+  );
+
+  const shouldNotRender =
+    archiveOpenedFrom === HISTORY || currentArchiveIndex === -1;
+
+  const nextArchive = archivesToBeSearched?.[currentArchiveIndex + 1];
+  const previousArchive = archivesToBeSearched?.[currentArchiveIndex - 1];
+
+  const onReadButtonClick =
+    (isNextArchiveButton = false) =>
+    async () => {
+      if (!isHistoryPage) {
+        addArchiveToHistory(archive);
+      }
+      const archiveToUse = isNextArchiveButton ? nextArchive : previousArchive;
+
+      if (!archiveToUse) {
+        return;
+      }
+
+      const archiveId = archiveToUse?.arcid;
+
+      document.getElementById(SCROLL_IMAGES_START_ID)?.scrollIntoView?.();
+      dispatch(updateCurrentArchiveId(archiveId));
+      setCurrentPageIndex(0);
+      dispatch(updateImagesScrollTarget(""));
+      await getNewArchivePages(archiveId);
+      putUpdateReadingProgression({ archiveId });
+    };
+
+  return {
+    onReadButtonClick,
+    shouldNotRender,
+  };
+};
+
+export default useTransitionButtonsLogic;
