40136e94c781c19eeefadc64cad25d3adaf397d0
Kibiki Kuze <desu2dan@gmail.com>
2026-01-11 19:15:00 -0600

    feat: add additional method for updating archive rating using keyboard

--- src/components/pages/images/image-page-rating.jsx
+++ src/components/pages/images/image-page-rating.jsx
@@ -1,19 +1,26 @@
 import { Grid2, Rating, TextField, Typography } from "@mui/material";
-import { useState } from "react";
+import { useCallback, useEffect, useId, useState } from "react";
 import { useArchiveRating } from "../../../hooks/useArchiveRating";
 import { isMobile } from "react-device-detect";
 import { LANRARAGI_STARS_ARRAY, RETURN_NULL } from "../../../constants";
 
+const VALID_RATINGS = ["1", "2", "3", "4", "5"];
+
 export const ImagePageRating = ({ postRating = RETURN_NULL }) => {
+  const id = useId();
+  const muiRatingComponentId = `image-page-rating-mui-component-${id}`;
   const isDesktopDevice = !isMobile;
   const [selectValue, setSelectValue] = useState("");
   const { updateArchiveRating } = useArchiveRating();
 
-  const handleRatingValueUpdate = (value) => {
-    setSelectValue(value);
-    updateArchiveRating(value);
-    postRating();
-  };
+  const handleRatingValueUpdate = useCallback(
+    (value) => {
+      setSelectValue(value);
+      updateArchiveRating(value);
+      postRating();
+    },
+    [postRating, updateArchiveRating]
+  );
 
   const onStarChange = (_event, value) => {
     handleRatingValueUpdate(LANRARAGI_STARS_ARRAY[value]);
@@ -23,6 +30,25 @@ export const ImagePageRating = ({ postRating = RETURN_NULL }) => {
     const value = event?.target?.value;
     handleRatingValueUpdate(value);
   };
+
+  const onKeyPress = useCallback(
+    (event) => {
+      const index = event?.key;
+      if (VALID_RATINGS.includes(index)) {
+        handleRatingValueUpdate(LANRARAGI_STARS_ARRAY[index]);
+      }
+    },
+    [handleRatingValueUpdate]
+  );
+
+  useEffect(() => {
+    const muiRatingElement = document.getElementById(muiRatingComponentId);
+    muiRatingElement.addEventListener("keydown", onKeyPress);
+
+    return () => {
+      muiRatingElement.removeEventListener("keydown", onKeyPress);
+    };
+  }, [muiRatingComponentId, onKeyPress]);
 
   return (
     <Grid2 id="rating-component" container spacing={1}>
@@ -36,6 +62,7 @@ export const ImagePageRating = ({ postRating = RETURN_NULL }) => {
           name="image-page-archive-rating"
           precision={1}
           size="large"
+          id={muiRatingComponentId}
           value={Number(selectValue?.length)}
           onChange={onStarChange}
           readOnly={!isDesktopDevice}
@@ -44,7 +71,7 @@ export const ImagePageRating = ({ postRating = RETURN_NULL }) => {
       {!isDesktopDevice && (
         <Grid2 className="flex justify-center" size={12}>
           <TextField
-            id="rate-archive-select-text-field"
+            id={`rate-archive-select-text-field`}
             className="h-full w-7/10"
             label="Rate Archive"
             fullWidth
